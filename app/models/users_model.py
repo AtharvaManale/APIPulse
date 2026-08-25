e93c4c8a93f3b2f7f8d6093add4218d57ec30905
Atharva <manaleatharva18@gmail.com>
2026-08-25 11:15:14 +0530

    DB model for users table

---
+++ app/models/users_model.py
@@ -0,0 +1,29 @@
+import uuid
+from datetime import datetime
+
+from db.database import Base
+from sqlalchemy import String, Integer, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, Relationship
+
+class Users(Base):
+    __tablename__ = "registered_users"
+
+    id : Mapped[str] = mapped_column(
+        String(255), primary_key=True, default=lambda: str(uuid.uuid4)
+    )
+
+    username : Mapped[str] = mapped_column(
+        String(50), nullable=False, unique=True
+    )
+
+    email_id : Mapped[str] = mapped_column(
+        String(200), nullable=False, unique=True
+    )
+
+    password : Mapped[str] = mapped_column(
+        String(255), nullable=False
+    )
+
+    created_at : Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow
+    )
