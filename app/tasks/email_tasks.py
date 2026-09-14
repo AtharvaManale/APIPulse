aee13d44dd9cb271104c270977d0bc414dd1e036
Atharva <manaleatharva18@gmail.com>
2026-09-14 19:06:26 +0530

    Celery worker: Retries for email task

--- app/tasks/email_tasks.py
+++ app/tasks/email_tasks.py
@@ -1,9 +1,16 @@
+import smtplib
 from app.dependencies.celery_dependency import celery
 from app.services.email_service import EmailService
 
 
-@celery.task(name="app.tasks.email_tasks.send_downtime_alert_email")
+@celery.task(
+    bind=True,
+    name="app.tasks.email_tasks.send_downtime_alert_email",
+    max_retries=3,
+    default_retry_delay=60
+)
 def send_downtime_alert_email(
+    self,
     to_email: str,
     username: str,
     api_name: str,
@@ -11,11 +18,14 @@ def send_downtime_alert_email(
     last_error: str,
     downtime_time: str | None = None
 ):
-    return EmailService.send_downtime_alert(
-        to_email=to_email,
-        username=username,
-        api_name=api_name,
-        api_url=api_url,
-        last_error=last_error,
-        downtime_time=downtime_time
-    )
+    try:
+        return EmailService.send_downtime_alert(
+            to_email=to_email,
+            username=username,
+            api_name=api_name,
+            api_url=api_url,
+            last_error=last_error,
+            downtime_time=downtime_time
+        )
+    except smtplib.SMTPException as exc:
+        raise self.retry(exc=exc, countdown=60)
